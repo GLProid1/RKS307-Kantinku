5860e513e1596899a6c4af243528a353a509f80e
GLProid1 <riconatanael2212@gmail.com>
2025-10-08 09:27:31 +0700

    Menambahkan endpoint update-status pada order

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CashConfirmView, CreateOrderView, MidtransWehboohView, OrderDetailView, CancelOrderView
+from .views import CashConfirmView, CreateOrderView, MidtransWehboohView, OrderDetailView, OrderCreateSerializer, CancelOrderView
 
 urlpatterns = [
   path("orders/create/", CreateOrderView.as_view(), name='create-order'),
   path("orders/<int:order_pk>/confirm-cash/", CashConfirmView.as_view(), name='confirm-cash'),
   path("webhooks/payment/", MidtransWehboohView.as_view(), name='payment-webhooks'),
   path('orders/<int:order_pk>/', OrderDetailView.as_view(), name='order-detail'),
-  path('orders/<int:order_pk>/cancel/', CancelOrderView.as_view(), name='cancel-order')
+  path('orders/<int:order_pk>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
+  path('orders/<int:order_pk>/update-status/' , UpdateOrderStatusView.as_view(), name='update-order-status'
 ]

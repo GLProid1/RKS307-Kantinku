fa7e1cc47d989e92070b8e2cfd228113731285c3
cincau476 <virrelj@gmail.com>
2025-10-09 12:19:52 +0700

    menambahkan webhooks

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,17 @@
+# Lokasi: orders/urls.py
+
 from django.urls import path
-from .views import CashConfirmView, CreateOrderView, MidtransWehboohView, OrderDetailView, UpdateOrderStatusView, CancelOrderView, OrderListView
+from .views import (
+    CashConfirmView, CreateOrderView, MidtransWehboohView, 
+    OrderDetailView, UpdateOrderStatusView, CancelOrderView, OrderListView
+)
 
 urlpatterns = [
-  path("orders/create/", CreateOrderView.as_view(), name='create-order'),
-  path("orders/<int:order_pk>/confirm-cash/", CashConfirmView.as_view(), name='confirm-cash'),
-   path('', OrderListView.as_view(), name='order-list'),
-  path("webhooks/payment/", MidtransWehboohView.as_view(), name='payment-webhooks'),
-  path('orders/<int:order_pk>/', OrderDetailView.as_view(), name='order-detail'),
-  path('orders/<int:order_pk>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
-  path('orders/<int:order_pk>/update-status/' , UpdateOrderStatusView.as_view(), name='update-order-status')
+    path('', OrderListView.as_view(), name='order-list'),
+    path("create/", CreateOrderView.as_view(), name='create-order'),
+    path("<int:order_pk>/", OrderDetailView.as_view(), name='order-detail'),
+    path("<int:order_pk>/confirm-cash/", CashConfirmView.as_view(), name='confirm-cash'),
+    path('<int:order_pk>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
+    path('<int:order_pk>/update-status/', UpdateOrderStatusView.as_view(), name='update-order-status'),
+    path("webhooks/payment/", MidtransWehboohView.as_view(), name='payment-webhooks'),
 ]

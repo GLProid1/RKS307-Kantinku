7ab207804f0966afb7a4e547bef703ed96660ffb
cincau476 <virrelj@gmail.com>
2025-10-13 14:21:40 +0700

    Menambahkan fitur qr dan image serta variant menu

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from .views import (
     CashConfirmView, CreateOrderView, MidtransWehboohView, 
-    OrderDetailView, UpdateOrderStatusView, CancelOrderView, OrderListView
+    OrderDetailView, UpdateOrderStatusView, CancelOrderView, OrderListView,TableQRCodeView, TakeawayQRCodeView
 )
 
 urlpatterns = [
@@ -13,5 +13,6 @@ urlpatterns = [
     path("<int:order_pk>/confirm-cash/", CashConfirmView.as_view(), name='confirm-cash'),
     path('<int:order_pk>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
     path('<int:order_pk>/update-status/', UpdateOrderStatusView.as_view(), name='update-order-status'),
-    path("webhooks/payment/", MidtransWehboohView.as_view(), name='payment-webhooks'),
+    path("webhooks/payment/", MidtransWehboohView.as_view(), name='payment-webhooks'),path("tables/<str:table_code>/qr/", TableQRCodeView.as_view(), name='table-qr-code'),
+    path("tenants/<int:tenant_id>/takeaway-qr/", TakeawayQRCodeView.as_view(), name='takeaway-qr-code'),
 ]

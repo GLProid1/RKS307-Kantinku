8db445a8c8b14c08c10da9dc525120eac3f2b78a
GLProid1 <riconatanael2212@gmail.com>
2025-11-29 07:48:18 +0700

    Menambahkan base endpoint api/ pada app tenants

--- canteen/urls.py
+++ canteen/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Semua endpoint API di bawah prefix 'api/
-    path('api/', include('orders.urls')),
-    path('tenants/', include('tenants.urls')),  # Tambahkan ini untuk meng-include URL dari aplikasi tenants
+    path('api/', include('orders.urls')), # orders.urls tidak perlu diubah
+    path('api/tenants/', include('tenants.urls')),  # Pindahkan tenants ke dalam /api/
     path('api/cashier/', include(('cashier.urls', 'cashier'))),  # Tambahkan ini untuk meng-include URL dari aplikasi cashier
     path('api/users/', include(('users.urls', 'users'))),  # Tambahkan ini untuk meng-include URL dari aplikasi users')
     

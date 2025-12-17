d000a60cddb6396ed769714ad42f11c03c800850
Virrel Juliansyah <virrelj@gmail.com>
2025-12-17 21:35:45 +0700

    nambahin celery

---
+++ canteen/celery.py
@@ -0,0 +1,19 @@
+# canteen/celery.py
+import os
+from celery import Celery
+
+# Set modul settings default Django untuk program 'celery'.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canteen.settings')
+
+app = Celery('canteen')
+
+# Menggunakan string di sini berarti worker tidak perlu pickle objek konfigurasi.
+# Namespace='CELERY' berarti semua config celery di settings.py harus berawalan 'CELERY_'
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules dari semua app yang terdaftar (seperti orders/tasks.py)
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')

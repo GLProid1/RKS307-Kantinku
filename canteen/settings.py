32ef866796b9204fe428f80c15268d8a1495b6a0
Virrel Juliansyah <virrelj@gmail.com>
2025-12-30 10:50:15 +0700

    p

--- canteen/settings.py
+++ canteen/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,192.168.1.10').split(',')
 
 
 # Application definition

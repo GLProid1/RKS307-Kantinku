545f5be97f2bc1e396de5fc2d3efe265daa2ef6f
virrel <virrelj@gmail.com>
2026-01-07 13:39:40 +0700

    Update urls.py

--- cashier/urls.py
+++ cashier/urls.py
@@ -3,5 +3,5 @@ from .views import CashConfirmView, VerifyOrderByPinView
 
 urlpatterns = [
      path("orders/verify-pin/", VerifyOrderByPinView.as_view(), name='verify-order-by-pin'),
-     path("orders/<uuid:order_uuid>/confirm-cash/", CashConfirmView.as_view(), name='confirm-cash'),
+     path("cash/confirm/<uuid:order_uuid>/", CashConfirmView.as_view(), name="cash-confirm")),
 ]
